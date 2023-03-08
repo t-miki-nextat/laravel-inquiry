a73bb5e2c9b777ac6233b9db3d5718861654e211
takeshi.miki <nextat_staff@adminnoMacBook-Pro.local>
2023-03-08 11:16:31 +0900

    add enum

---
+++ laravel-inquiry/app/Enums/InquiryType.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Enums;
+
+
+enum InquiryType: string
+{
+    case ESTIMATE = 'お見積り';
+    case RECRUIT = '採用';
+    case OTHER = 'その他';
+
+    /**
+     *
+     */
+    public function text(): string
+    {
+        return match($this) {
+            self::ESTIMATE => 'お見積り',
+            self::RECRUIT => '採用',
+            self::OTHER => 'その他',
+        };
+    }
+}
+
