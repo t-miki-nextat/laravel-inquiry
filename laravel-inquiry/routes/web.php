ca95edebdb9ef9d17de4eeb56e837015fe43d94e
takeshi.miki <nextat_staff@adminnoMacBook-Pro.local>
2023-03-14 09:48:38 +0900

    Add routing and controller

--- laravel-inquiry/routes/web.php
+++ laravel-inquiry/routes/web.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InquiryController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,9 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
     });
     Route::resource('admin', AdminController::class);
 });
+
+Route::get('admin/users', [AdminUserController::class, 'index'])->name('index');
+Route::post('admin/users', [AdminUserController::class, 'register'])->name('register');
+Route::get('admin/users/{id}', [AdminUserController::class, 'show'])->name('show');
+Route::put('admin/users/{id}', [AdminUserController::class, 'update'])->name('update');
+Route::delete('admin/users/{id}', [AdminUserController::class, 'delete'])->name('delete');
