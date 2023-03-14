9b0c25c999c3adaf89977a4f23e465e9c3fe1a5a
takeshi.miki <nextat_staff@adminnoMacBook-Pro.local>
2023-03-14 16:29:28 +0900

    Use InquiryMailService

--- laravel-inquiry/app/Http/Controllers/InquiryController.php
+++ laravel-inquiry/app/Http/Controllers/InquiryController.php
@@ -4,22 +4,21 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Enums\InquiryType;
 use App\Models\Inquiry;
 use App\Models\User;
-use App\Notifications\InquiryNotification;
-use App\Services\InquiryService;
-use Illuminate\Contracts\Foundation\Application;
+use App\Services\InquiryMailService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Redirector;
-use Illuminate\Validation\Rules\Enum;
 use Illuminate\View\View;
 use App\Http\Requests\StoreInquiryRequest;
-use Illuminate\Support\Facades\Notification;
 
 class InquiryController extends Controller
 {
+
+    public function __construct(private readonly InquiryMailService $service)
+    {
+    }
+
     /**
      * Display an inquiry form.
      *
@@ -61,9 +60,11 @@ class InquiryController extends Controller
 
         $inquiry->fill($validated)->save();
 
-        $users = User::all();
-        $content = $inquiry->content;
-        Notification::send($users, new InquiryNotification($content));
+        /** @var User $user */
+        foreach (User::query()->cursor() as $user) {
+            $content = $inquiry->content;
+            $this->service->send($user, $content);
+        }
 
         return redirect()->route("inquiries.complete");
     }
@@ -100,3 +101,5 @@ class InquiryController extends Controller
         //
     }
 }
+
+
